4d26a1b94b86e22e289488c82eab984352ee4349
duwei <905571995@qq.com>
2016-08-16 09:33:24 +0800

    修改自己的资料 和 post 不需要验证权限

--- api/v1/user/index.js
+++ api/v1/user/index.js
@@ -15,7 +15,14 @@ const checkPermission = require('../../../components/checkPermission');
 router.get('/', canLogin, checkPermission, Ctrl.index);
 router.get('/:id', canLogin, checkPermission, Ctrl.show);
 router.post('/', mustLogin, checkPermission, Ctrl.create);
-router.put('/:id', mustLogin, checkPermission, Ctrl.update);
+router.put('/:id', mustLogin, function(req, res, next) {
+  // 修改自己的资料不需要permission验证
+  if (req.params.id && (req.currentUser._id.toString() === req.params.id.toString())) {
+    next();
+  } else {
+    checkPermission();
+  }
+}, Ctrl.update);
 router.delete('/:id', mustLogin, checkPermission, Ctrl.destroy);
 
 module.exports = router;
