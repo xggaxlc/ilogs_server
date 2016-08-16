4ddccaee352cb3dd67a38869d84b8f2ffcc8c465
duwei <905571995@qq.com>
2016-08-16 20:40:41 +0800

    node 环境默认设置development

--- config/environment/index.default.js
+++ config/environment/index.default.js
@@ -2,16 +2,8 @@
 
 const _ = require('lodash');
 
-(function() {
-  if (!process.env.NODE_ENV) {
-    throw new Error('你需要设置运行环境！');
-  }
-  return process.env.NODE_ENV;
-})();
-
-
 let all = {
-  env: process.env.NODE_ENV,
+  env: process.env.NODE_ENV || 'development',
   port: process.env.PORT || 9000,
   ip: process.env.IP || '0.0.0.0',
   host: 'http://localhost:9000',
