eff9d9286b6f161452a9c3f8ab608cb8890e7968
duwei <905571995@qq.com>
2016-09-07 13:21:20 +0800

    权限检查 调整

--- components/checkPermission.js
+++ components/checkPermission.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Role = require('../api/v1/role/role.model');
+
 module.exports = function(req, res, next) {
   //无需登录验证的API，不用验证权限
   if (!req.currentUser) return next();
@@ -10,9 +12,14 @@ module.exports = function(req, res, next) {
   } else {
     let api = req.baseUrl.split('/').pop().toLowerCase();
     let method = req.method.toLowerCase();
-    let permissions = req.currentUser.role.permissions.toObject();
+    let permissions = new Role({}).toObject();
     if (permissions[api] && permissions[api]['hasOwnProperty'](method)) {
-      permissions[api][method] ? next() : res.status(403).end();
+      try{
+        let userPermissions = req.currentUser.role.permissions.toObject();
+        userPermissions[api][method] ? next() : res.status(403).end();
+      } catch(e) {
+        res.status(403).end();
+      }
     } else {
       //model没有这个权限字段(默认都有无需设置权限)
       next();
